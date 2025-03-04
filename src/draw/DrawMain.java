f7267bcd2e441c2a5602de0e59be0dbaf1a9db7c
MisterGreenDino <mistergreendino@gmail.com>
2025-03-04 16:30:11 +0100

    commit to main
    commit

---
+++ src/draw/DrawMain.java
@@ -0,0 +1,35 @@
+package draw;
+
+import static org.lwjgl.opengl.GL11.*;
+import java.util.List;
+
+import map.Entity;
+import player.Player;
+import player.Camera;
+import map.Map;
+
+public class DrawMain {
+    static float ScreenWidth;
+    static float ScreenHeight;
+    static Camera camera;
+
+    public static float[] convertCartesianToGL(float x, float y) {
+        float adjustedX = x - camera.getX() + ScreenWidth/2;
+        float adjustedY = -(y - camera.getY()) + ScreenHeight/2;
+        return new float[]{adjustedX, adjustedY};
+    }
+
+    public static void drawAll(Player player, Map map, float screenWidth, float screenHeight, Camera cam) {
+        ScreenWidth = screenWidth;
+        ScreenHeight = screenHeight;
+        camera = cam;
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        DrawRoom.draw(map);
+
+        float[] glCoords = convertCartesianToGL(player.getX(), player.getY());
+
+
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -13,7 +13,97 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+<<<<<<< Updated upstream
 public class Main {
+=======
+public class Main{
+	private static final int TARGET_FPS = 60; // Set TARGET_FPS to -1 if you don't want to limit
+	private static final double TARGET_TIME = 1/(double)TARGET_FPS;
+	
+	private Window window;
+	private int screenWidth;
+	private int screenHeight;
+	private double dt;
+	private int currentFPS;
+	// For the Game
+	private Player player;
+	private Map map;
+	private Camera camera;
+
+	private void run() {
+		this.init();
+		this.loop();
+		this.terminate();
+	}
+	
+	private void init() {
+		//Start lwjgl
+		GLFWErrorCallback.createPrint(System.err).set();
+		if (!glfwInit()) {
+			throw new IllegalStateException("Unable to initialize GLFW");
+		}
+
+		// Create Window
+		GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		screenWidth = vidmode.width();
+		screenHeight = vidmode.height();
+		this.window = new Window(screenWidth, screenHeight, "Y");
+		this.window.create();
+
+		//Instance game data
+		player = new Player(0, 700, 600);
+
+		map = new Map();
+		Room room1 = new Room(0, 0, 300, 300, 500, 200);
+		Room room2 = new Room(1, 0, 600, 600, 5500, 500);
+		map.addRoom(room1);
+		map.addRoom(room2);
+
+		Wall wall1 = new Wall(150, 150, 200, 200); // (x, y, width, height)
+		room2.addWall(wall1);
+
+		camera = new Camera(player);
+		
+	}
+	
+	private void loop() {
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glOrtho(0, screenWidth, screenHeight, 0, -1, 1);
+		long lastTime = System.nanoTime()/1000;
+		
+		while (!window.shouldClose()) {
+			
+			long currentTime = System.nanoTime()/1000;
+			this.dt = (currentTime-lastTime)/1000.0;
+			
+			if ( TARGET_TIME <= this.dt ) {
+				this.currentFPS = (int) (1/this.dt);
+				this.render();
+				this.update();
+				lastTime = System.nanoTime()/1000;
+			}
+		}
+		
+	}
+	
+	private void terminate() {
+		this.window.delete();
+		// Terminate GLFW and free the error callback
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		DrawMain.drawAll(player, map, screenWidth, screenHeight, camera);
+	}
+	
+	private void update() {
+		this.player.update(window.getWindow(), map);
+		this.camera.update(map, screenHeight, screenWidth);
+		this.window.update();
+	}
+>>>>>>> Stashed changes
 	
 	public static void main(String[] args) {
 		new Main().run();
